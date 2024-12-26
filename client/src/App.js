89cb809a7ecf52c39c50404b9a7b50b2c27d9739
NayakYadav1 <yadavnayak202@gmail.com>
2024-12-26 18:52:53 +0545

    Register user

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element= {<HomePage />} />
-            <Route path='/login' element= {<Login />} />
-            <Route path='/register' element= {<Register />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    </div>
+    <ConfigProvider wave={{ disabled: true }}>
+      <div className="App">
+        <>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element= {<HomePage />} />
+              <Route path='/login' element= {<Login />} />
+              <Route path='/register' element= {<Register />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      </div>
+    </ConfigProvider>
   );
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,38 +1,55 @@
-import React from 'react'
-import '../styles/RegisterStyles.css'
-import { Button, Form, Input } from 'antd'
-import { Link } from 'react-router-dom'
-// import Form from 'antd/es/form/Form'
+  import React from 'react'
+  import '../styles/RegisterStyles.css'
+  import { Link, useNavigate } from 'react-router-dom'
+  import axios from 'axios';
+  import Form from 'antd/es/form/Form';
+  import { Button, Input, message } from 'antd';
 
-const Register = () => {
-  
-  // Form Handler
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const Register = () => {
+    const navigate = useNavigate()
+    
+    // Form Handler
+    const onFinishHandler = async(values) => {
+      console.log(values);
+      
+      try{
+        const res = await axios.post('/api/v1/user/register', values);
+        console.log(res.data);
+        
+        if(res.data.success) {
+          message.success("Registered Successfully");
+          navigate('/login');
+        } else {
+          message.error(res.data.message);
+        }
+      } catch(error) {
+        console.log(error);
+        message.error('Something went wrong')
+      }
+    }
+
+    return (
+      <>
+        <div className='form-container'>
+          <Form layout='vertical' onFinish={onFinishHandler} className='register-form'>
+            <h3 className='text-center'>Register</h3>
+            <Form.Item  label="Name" name="name">
+              <Input type='text' required />
+            </Form.Item>
+            <Form.Item  label="Email" name="email">
+              <Input type='Email' required />
+            </Form.Item>
+            <Form.Item  label="Password" name="password">
+              <Input type='password' required />
+            </Form.Item>
+            <Link to='/login' className='m-2'>Already user login here</Link>
+            <Button className='btn btn-primary' type='submit'>
+              Register
+            </Button>
+          </Form>
+        </div>
+      </>
+    )
   }
 
-  return (
-    <>
-      <div className='form-container'>
-        <Form layout='vertical' onFinish={onfinishHandler} className='register-form'>
-          <h3 className='text-center'>Register</h3>
-          <Form.Item  label="Name" name="name">
-            <Input type='text' required />
-          </Form.Item>
-          <Form.Item  label="Email" name="email">
-            <Input type='Email' required />
-          </Form.Item>
-          <Form.Item  label="Password" name="password">
-            <Input type='password' required />
-          </Form.Item>
-          <Link to='/login' className='m-2'>Already user login here</Link>
-          <button className='btn btn-primary' type='submit'>
-            Register
-          </button>
-        </Form>
-      </div>
-    </>
-  )
-}
-
-export default Register
+  export default Register
