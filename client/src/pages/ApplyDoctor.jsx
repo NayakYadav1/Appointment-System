2dc845ebe010bfa7889ef602c195b0af5068b7be
NayakYadav1 <yadavnayak202@gmail.com>
2025-01-06 14:29:56 +0545

    Doctor model and apply doctor form being made ready

---
+++ client/src/pages/ApplyDoctor.jsx
@@ -0,0 +1,121 @@
+import Layout from "../components/Layout";
+import React from "react";
+import { Col, Form, Input, Row, TimePicker } from "antd";
+
+const ApplyDoctor = () => {
+  // handle form
+  const handleFinish = (values) => {
+    console.log(values);
+  }
+  return (
+    <Layout>
+      <h1 className="text-center">Apply Doctor</h1>
+      <Form layout="vertical" onFinish={handleFinish} className="m-3">
+        <h4 className="">Personal Details : </h4>
+        <Row gutter={20}>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="First Name"
+              name="firstName"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your First Name" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Last Name"
+              name="lastName"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Last Name" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Phone"
+              name="phone"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Name Phone Number" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Email"
+              name="email"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Email" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item label="Website" name="website">
+              <Input type="text" placeholder="Your Website" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Address"
+              name="address"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Address" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <h4 className="">Professional Details : </h4>
+        <Row gutter={20}>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Specialization"
+              name="specialization"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Specialization" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Experience"
+              name="experience"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Experience" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item
+              label="Fee Per Consaltation"
+              name="feePerConsaltation"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Your Fee Per Consaltation" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} lg={8}>
+            <Form.Item label="Timings" name="timings" >
+              <TimePicker.RangePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <div className="d-flex justify-content-end mb-3">
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </div>
+      </Form>
+    </Layout>
+  );
+};
+
+export default ApplyDoctor;
---
+++ models/doctorModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+    },
+    firstName: {
+        type: String,
+        required: [true, 'First Name is required'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last Name is required'],
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+    },
+    website: {
+        type: String,
+    },
+    address: {
+        type: String,
+        required: [true, 'Address is required'],
+    },
+    specialization: {
+        type: String,
+        requires: [true, 'Specialization is required'],
+    },
+    experience: {
+        type: String,
+        required: [true, 'Experience is required'],
+    },
+    feesPerConsaltation: {
+        type: Number,
+        required: [true, 'Fee is required'],
+    },
+    timings: {
+        type: Object,
+        required: [true, 'Work timing is required'],
+    },
+}, { timestamps: true })
+
+const doctorModel = mongoose.model('users', doctorSchema)
+module.exports = doctorModel;
