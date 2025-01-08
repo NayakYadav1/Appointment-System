549fe918f75ea2dbb1d0da3a09b9a3913711474a
NayakYadav1 <yadavnayak202@gmail.com>
2025-01-08 20:34:33 +0545

    Problem related with the Route of doctor resolved

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Doctors from "./pages/admin/Doctors";
 import Profile from './pages/doctor/Profile';
 
 function App() {
-  const { loading } = useSelector((state) => state.alerts);
+  const { loading } = useSelector(state => state.alerts);
   return (
     <ConfigProvider wave={{ disabled: true }}>
       <>
